d62ad106c1c4a447004dd43a0edbd37e527b1cf4
LEAFERx <jerryleafer@outlook.com>
2017-02-21 21:46:29 +0800

    update main py

--- book-to-blog.py
+++ book-to-blog.py
@@ -2,14 +2,23 @@ from urllib import request
 import json
 
 while 1:
-    isbn = input()
+    booklistpath = input("Input path to booklist:")
     try:
-        f = request.urlopen("https://api.douban.com/v2/book/isbn/" + isbn)
-        s = f.read()
-        s = s.decode("utf-8")
-        js = json.loads(s)
-        print(js['code'])
-    except Exception as e:
-        print(e)
-        
-    
+        f = open(booklistpath, "r")
+        isbns = f.read()
+        isbnlist = isbns.split('<br>')
+        for isbn in isbnlist:
+            try:
+                w = request.urlopen("https://api.douban.com/v2/book/isbn/" + isbn)
+                s = w.read()
+                s = s.decode("utf-8")
+                js = json.loads(s)
+                print(js['alt'])
+            except BaseException as e:
+                print("Error occured when processing: " + isbn + "\n")
+                print(e)
+    except BaseException as e:
+        print("Path to Booklist is invalid: " + e)
+    finally:
+        if f:
+            f.close()
